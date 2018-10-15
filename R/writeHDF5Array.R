4564677f0993f92e49fac0fbc2b2611c77cf3783
Tan <ptdtan@gmail.com>
2018-10-15 18:02:39 +0700

    downgrade to last function in DelayedArray write_array_to_sink

--- R/writeHDF5Array.R
+++ R/writeHDF5Array.R
@@ -172,7 +172,7 @@ writeHDF5Array <- function(x, filepath=NULL, name=NULL, chunkdim=NULL,
         old_verbose <- DelayedArray:::set_verbose_block_processing(verbose)
         on.exit(DelayedArray:::set_verbose_block_processing(old_verbose))
     }
-    BLOCK_write_to_sink(x, sink)
+    write_array_to_sink(x, sink)
     as(sink, "HDF5Array")
 }
 
